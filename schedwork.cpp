852f63a93a0c6fcc0200ebea18beffc9513a965f
Miya Shin <mmshin@usc.edu>
2023-04-03 19:39:13 +0000

    Final

--- schedwork.cpp
+++ schedwork.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring> // delete if necessary
 #include <vector>
 #include <map>
 #include <algorithm>
@@ -24,21 +25,90 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add your implementation of schedule() and other helper functions here
 
+bool isWorkerScheduled(
+    const Worker_T& currWorker,
+    const vector<vector<Worker_T>>& sched,
+    const int& day
+) {
+    if (sched[day].empty()) {
+        return true;
+    }
+    else {
+        for (const auto& worker : sched[day]) {
+            if (worker == currWorker) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void scheduleHelper(
+    const AvailabilityMatrix& avail,
+    vector<vector<Worker_T>>& sched,
+    int shiftNum[],
+    int& day,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    bool& valid
+) {
+    for (size_t i = 0; i < avail[0].size(); ++i) {
+        Worker_T currWorker = static_cast<Worker_T>(i);
+        if (avail[day][i] && shiftNum[currWorker] < static_cast<int>(maxShifts) && isWorkerScheduled(currWorker, sched, day)) {
+            ++shiftNum[currWorker];
+            sched[day].push_back(currWorker);
+            if (sched[day].size() == dailyNeed) {
+                if (day < static_cast<int>(avail.size() - 1)) {
+                    ++day;
+                    scheduleHelper(avail, sched, shiftNum, day, dailyNeed, maxShifts, valid);
+                    if (valid) {
+                        return;
+                    }
+                    else {
+                        --shiftNum[currWorker];
+                        --day;
+                        sched[day].pop_back();
+                    }
+                }
+                else {
+                    valid = true;
+                    return;
+                }
+            }
+            else {
+                scheduleHelper(avail, sched, shiftNum, day, dailyNeed, maxShifts, valid);
+                if (valid) {
+                    return;
+                }
+                else {
+                    sched[day].pop_back();
+                    --shiftNum[currWorker];
+                }
+            }
+        }
+    }
+}
+
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
     const size_t maxShifts,
     DailySchedule& sched
-)
-{
-    if(avail.size() == 0U){
+) {
+    if (avail.empty()) {
         return false;
     }
     sched.clear();
-    // Add your code below
-
-
-
-
+    vector<Worker_T> vecId;
+    bool valid = false;
+    int day = 0;
+    for (size_t i = 0; i < avail.size(); ++i) {
+        sched.push_back(vecId);
+    }
+    int shiftNum[avail[0].size()];
+    memset(shiftNum, 0, sizeof(shiftNum));
+    scheduleHelper(avail, sched, shiftNum, day, dailyNeed, maxShifts, valid);
+    return valid;
 }
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -11,18 +11,118 @@
 #include "dict-eng.h"
 using namespace std;
 
+bool wordleWordCheck(const std::string& floating, const std::set<std::string>& dict, const std::string& curr);
+void wordleHelperFunc(const std::string& in, const std::string& floating, 
+const std::set<std::string>& dict, std::set<std::string>& mySet, std::string curr, int cnt);
 
-// Add prototypes of helper functions here
-
-
-// Definition of primary wordle function
+// Primary wordle function definition
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
 
+  // Prototypes of helper functions
+  bool wordleWordCheck(const std::string& floating, const std::set<std::string>& dict, const std::string& curr);
+  void wordleHelperFunc(const std::string& in, const std::string& floating, 
+    const std::set<std::string>& dict, std::set<std::string>& mySet, std::string curr, int cnt);
+
+  // The code starts here
+  std::set<std::string> mySet;
+  bool check = true;
+  std::string curr = "";
+  int cnt = 0;
+
+  // Count the number of open spots in the input string
+  for (size_t i = 0; i < in.length(); ++i) {
+    if (in[i] == '-') {
+      cnt++;
+      check = false;
+    }
+  }
+
+  // Case 1: if input string is empty
+  if (in.empty()) { 
+    return mySet;
+  }
+
+  // Case 2: if the input string is complete
+  else if (check == true) {
+    mySet.insert(in);
+  }
+
+  // Case 3: not solved yet
+  else {
+    wordleHelperFunc(in, floating, dict, mySet, curr, cnt);
+  }
+
+  return mySet;
 }
 
-// Define any helper functions here
+// Helper function to check if the word can be made with floating letters
+bool wordleWordCheck(const std::string& floating, const std::set<std::string>& dict, const std::string& curr)
+{
+  int cnt = 0;
+  std::string currString = curr;
+
+  for (int i = floating.length() - 1; i >= 0; --i) {
+    char floatingChar = floating[i];
+    size_t currLoc = currString.find(floatingChar);
+
+    // If the character is found
+    if (currLoc != string::npos) {
+      currString.erase(currString.begin() + (int)currLoc);
+      cnt++;
+    }
+  }
+
+  if (dict.find(curr) == dict.end()) {
+    return false;
+  }
+  
+  if ((unsigned int)cnt != floating.length()) {
+    return false;
+  }
+
+  return true;
+}
+
+// Helper function to generate all possible combinations
+void wordleHelperFunc(const std::string& in, const std::string& floating, const std::set<std::string>& dict, 
+  std::set<std::string>& mySet, std::string curr, int cnt)
+{
+  // Case 1: length of the string is the same as the input string
+  if (curr.length() == in.length()) { 
+    if (wordleWordCheck(floating, dict, curr)) {
+      mySet.insert(curr);
+    }
+    return;
+  }
+
+// Case 2: if the character at point is already figured out (not a dash)
+if (in[curr.length()] != '-') {
+std::string myString = curr + in[curr.length()];
+wordleHelperFunc(in, floating, dict, mySet, myString, cnt);
+}
+
+// Case 3: if all floating letters are used up
+else if (cnt == (int)floating.length()) {
+for (int i = floating.length() - 1; i >= 0; --i) {
+std::string myFloatingString = floating;
+std::string currCopy = curr + myFloatingString[i];
+myFloatingString.erase(i, 1);
+wordleHelperFunc(in, myFloatingString, dict, mySet, currCopy, cnt - 1);
+}
+}
+
+// Case 4: if there are still floating letters left
+else {
+for (char i = 'a'; i <= 'z'; ++i) {
+std::string myFloatingString = floating;
+if (myFloatingString.find(i) != string::npos) {
+myFloatingString.erase(myFloatingString.find(i), 1);
+}
+wordleHelperFunc(in, myFloatingString, dict, mySet, curr + i, cnt - 1);
+}
+}
+}
